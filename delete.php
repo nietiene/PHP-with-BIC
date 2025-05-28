fe76c4ceb8c6674ea9f24048a12cc2a1b86af86b
nietiene <niyomugaboetiene53@gmail.com>
2025-05-28 09:19:55 -0700

    php commit

--- delete.php
+++ delete.php
@@ -1,10 +1,11 @@
 <?php
-include("conn.php")
-$id = $_GET['id'];
-$delete ="DELETE FROM patient WHERE id = ?";
-$stmt = $conn -> prepare($delete);
-$stmt -> execute([$id]);
-header("Location:select.php");
+include("conn.php");
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $delete ="DELETE FROM patient WHERE id = ?";
+    $stmt = $conn -> prepare($delete);
+    $stmt -> execute([$id]);
+    header("Location:select.php");
 
-
+}
 ?>
